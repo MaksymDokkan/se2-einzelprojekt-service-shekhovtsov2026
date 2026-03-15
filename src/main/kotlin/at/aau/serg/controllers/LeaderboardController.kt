68be3b98ab6bc8fac591c1d2c5805db98e119c73
Maksym-Shekhovtsov <maksmsh@edu.aau.at>
2026-03-15 21:28:34 +0100

    Updated The getLeaderboard to support rank return.

--- src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
+++ src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
@@ -2,9 +2,12 @@ package at.aau.serg.controllers
 
 import at.aau.serg.models.GameResult
 import at.aau.serg.services.GameResultService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/leaderboard")
@@ -13,7 +16,35 @@ class LeaderboardController(
 ) {
 
     @GetMapping
-    fun getLeaderboard(): List<GameResult> =
-        gameResultService.getGameResults().sortedWith(compareBy({ -it.score }, {it.timeInSeconds }))
+    fun getLeaderboard( @RequestParam(required = false) rank: Int? = null): List<GameResult> {
 
+
+        //2.2.1 Sortierung
+        //  gameResultService.getGameResults().sortedWith(compareBy({ -it.score }, {it.timeInSeconds }))
+
+        val rankBand = 3
+
+        val Leaderboard = gameResultService.getGameResults().sortedWith(
+            compareBy({ -it.score }, { it.timeInSeconds })
+        )
+
+        if (rank == null)
+        {
+         return Leaderboard
+        }
+
+        if (rank <= 0 ) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid rank, minimum zero.")
+        }
+        if ( rank > Leaderboard.size) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid rank, higher then player count.")
+        }
+
+
+        val start = maxOf(0, rank - (1+rankBand))
+
+        val end = minOf(Leaderboard.size, rank + (rankBand))
+
+        return Leaderboard.subList(start, end)
+    }
 }
