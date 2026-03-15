9c22e95a7923a3594b456d47eb8b8791adc55c46
Maksym-Shekhovtsov <maksmsh@edu.aau.at>
2026-03-15 21:53:43 +0100

    Added Testing to the new rank return.

--- src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
+++ src/test/kotlin/at/aau/serg/controllers/LeaderboardControllerTests.kt
@@ -3,8 +3,11 @@ package at.aau.serg.controllers
 import at.aau.serg.models.GameResult
 import at.aau.serg.services.GameResultService
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ResponseStatusException
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import org.mockito.Mockito.`when` as whenever // when is a reserved keyword in Kotlin
@@ -53,5 +56,82 @@ class LeaderboardControllerTests {
         assertEquals(third, res[1])
         assertEquals(first, res[2])
     }
+
+
+    private fun setupMockedLeaderboard(size: Int) {
+        val results = (1..size).map {
+            GameResult(it.toLong(), "Player$it", 100 - it, it.toDouble())
+        }
+        whenever(mockedService.getGameResults()).thenReturn(results)
+    }
+
+
+    @Test
+    fun test_getLeaderboard_invalidRankZero_throwsException() {
+        setupMockedLeaderboard(1)
+
+        val exception = assertThrows<ResponseStatusException> {
+            controller.getLeaderboard(0)
+        }
+        assertEquals(HttpStatus.BAD_REQUEST, exception.statusCode)
+    }
+
+    @Test
+    fun test_getLeaderboard_invalidRankNegative_throwsException() {
+        setupMockedLeaderboard(1)
+
+        val exception = assertThrows<ResponseStatusException> {
+            controller.getLeaderboard(-5)
+        }
+        assertEquals(HttpStatus.BAD_REQUEST, exception.statusCode)
+    }
+
+    @Test
+    fun test_getLeaderboard_invalidRankTooHigh_throwsException() {
+        setupMockedLeaderboard(1)
+
+        val exception = assertThrows<ResponseStatusException> {
+            controller.getLeaderboard(2)
+        }
+        assertEquals(HttpStatus.BAD_REQUEST, exception.statusCode)
+    }
+
+    @Test
+    fun test_getLeaderboard_rankInMiddle_returnsSevenPlayers() {
+
+        setupMockedLeaderboard(10)
+
+        val res = controller.getLeaderboard(5)
+
+        assertEquals(7, res.size)
+        assertEquals("Player2", res.first().playerName)
+        assertEquals("Player5", res[3].playerName)
+        assertEquals("Player8", res.last().playerName)
+    }
+
+    @Test
+    fun test_getLeaderboard_rankNearTop_hitsStartBoundary() {
+        setupMockedLeaderboard(10)
+
+        val res = controller.getLeaderboard(2)
+
+        assertEquals(5, res.size)
+        assertEquals("Player1", res.first().playerName)
+        assertEquals("Player5", res.last().playerName)
+    }
+
+    @Test
+    fun test_getLeaderboard_rankNearBottom_hitsEndBoundary() {
+        setupMockedLeaderboard(10)
+
+        val res = controller.getLeaderboard(9)
+
+        assertEquals(5, res.size)
+        assertEquals("Player6", res.first().playerName)
+        assertEquals("Player10", res.last().playerName)
+    }
+
+
+
 
 }
